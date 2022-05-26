e10c3d1a9b71863082ee3d4f3a050d9c8c0aca23
sanjeevprabhakar <sanjeevprabhakar2021@gmail.com>
2022-05-26 14:56:40 -0600

    adding files of simple program

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int n1 = 0;
+            int n2 = 1;
+            int number;
+            int n3;
+
+            Console.WriteLine("here are your series");
+            
+
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2);
+
+            for (int i = 3; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3+ " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
